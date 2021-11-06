b38915aa8f89e579070cf6a2ae180972ea65a022
Loza Marusa <marusa.loza@gmail.com>
2021-11-06 19:29:29 +0200

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+Implement Announcement Website with the following functionality:
+
+1. Add new announcement
+2. Edit announcement
+3. Delete announcement
+4. See list of announcement
+5. Search announcements by title
+6. Show selected announcement details
+
+[DEMO LINK](https://MarusaLoza.github.io/Announcements-Angular/)
---
+++ src/app/announ-list.ts
@@ -0,0 +1,14 @@
+export const Announcement = [
+  {title: 'Announcement 1',
+    description: 'Lorem Ipsum.',
+    nowDate: new Date
+  },
+  {title: 'Announcement 2',
+    description: 'It is a long layout.',
+    nowDate: new Date
+  },
+  {title: 'Announcement 3',
+    description: 'Been the industrys.',
+    nowDate: new Date
+  }
+];
